35d45c2ecbdbccc11cd684cf55ba228785eef131
Halil Zilif <93616680+Zhalil24@users.noreply.github.com>
2025-05-29 09:37:53 +0300

    Update

--- lib/feature/drawer/view/widget/input_dialog_widget.dart
+++ lib/feature/drawer/view/widget/input_dialog_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hipocapp/product/utility/validator/validator.dart';
 import 'package:hipocapp/product/widget/button/custom_action_button/custom_action_button.dart';
+import 'package:kartal/kartal.dart';
 
 class InputDialogWidget extends StatefulWidget {
   final void Function(String titleText, String descText) onSubmit;
@@ -24,61 +25,63 @@ class _InputDialogWidgetState extends State<InputDialogWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      content: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 8),
-            TextFormField(
-              validator: Validators.notEmpty,
-              controller: widget.titleController,
-              decoration: const InputDecoration(labelText: 'Başlık'),
-              onChanged: (value) {
-                setState(() {
-                  titleText = value;
-                });
-              },
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              validator: Validators.notEmpty,
-              controller: widget.descController,
-              decoration: const InputDecoration(
-                labelText: 'Entry',
-                alignLabelWithHint: true,
-                border: OutlineInputBorder(),
+    return SingleChildScrollView(
+      child: AlertDialog(
+        content: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: context.sized.lowValue),
+              TextFormField(
+                validator: Validators.notEmpty,
+                controller: widget.titleController,
+                decoration: const InputDecoration(labelText: 'Başlık'),
+                onChanged: (value) {
+                  setState(() {
+                    titleText = value;
+                  });
+                },
               ),
-              minLines: 10,
-              maxLines: null,
-              onChanged: (value) {
-                setState(() {
-                  descText = value;
-                });
-              },
-            ),
-          ],
+              SizedBox(height: context.sized.lowValue),
+              TextFormField(
+                validator: Validators.notEmpty,
+                controller: widget.descController,
+                decoration: const InputDecoration(
+                  labelText: 'Entry',
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(),
+                ),
+                minLines: 10,
+                maxLines: null,
+                onChanged: (value) {
+                  setState(() {
+                    descText = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('İptal'),
+          ),
+          CustomActionButton(
+            text: 'Gönder',
+            onTop: () {
+              if (_formKey.currentState?.validate() ?? false) {
+                final title = widget.titleController.text.trim();
+                final desc = widget.descController.text.trim();
+                widget.onSubmit(title, desc);
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ],
       ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text('İptal'),
-        ),
-        CustomActionButton(
-          text: 'Gönder',
-          onTop: () {
-            if (_formKey.currentState?.validate() ?? false) {
-              final title = widget.titleController.text.trim();
-              final desc = widget.descController.text.trim();
-              widget.onSubmit(title, desc);
-              Navigator.of(context).pop();
-            }
-          },
-        ),
-      ],
     );
   }
 }
