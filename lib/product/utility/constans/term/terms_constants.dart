349a5a929223f3fe2985c08a5a120aaca7080c16
Halil Zilif <93616680+Zhalil24@users.noreply.github.com>
2025-06-11 16:40:19 +0300

    kvkk added

--- lib/product/utility/constans/term/terms_constants.dart
+++ lib/product/utility/constans/term/terms_constants.dart
@@ -1,14 +1,33 @@
 class TermsConstants {
   static const String termsAndConditions = '''
-  Lütfen aşağıdaki şartları dikkatlice okuyunuz.
+  Kişisel Verilerin Korunması Hakkında Bilgilendirme Metni
 
-  - Kullanıcı, verdiği bilgilerin doğru olduğunu kabul eder.
-  - Uygulama, kişisel verileri KVKK'ya uygun olarak saklayacaktır.
-  - Bu uygulama üzerinden yapılan tüm işlemler kayıt altına alınabilir.
-  - Kullanıcı, hizmeti sadece kişisel kullanımı için kullanmayı kabul eder.
-  - Geliştirici, herhangi bir zamanda şartları değiştirme hakkını saklı tutar.
-  - Detaylı bilgi için lütfen www.ornekwebsitesi.com adresini ziyaret edin.
+Hipocapp.com olarak, kullanıcılarımızın kişisel verilerinin korunmasına büyük önem veriyoruz. Bu metin, kullanıcıların kişisel verilerinin toplanması, işlenmesi ve korunması ile ilgili olarak bilgilendirilmesini amaçlamaktadır.
 
-  Bu metin, sadece örnek amaçlıdır ve gerçek bir sözleşme yerine geçmez.
+1. Kişisel Verilerin Toplanması ve İşlenmesi
+
+Kullanıcılarımızın kişisel verileri, sağlıkla ilgili bilgi paylaşımı ve mesajlaşma özelliği kapsamında toplanmaktadır. Bu veriler, kullanıcıların telefon numaraları, e-posta adresleri gibi iletişim bilgilerini içermektedir. Kullanıcılar, bu bilgilerin işlenmesine açık rızaları ile onay vermektedir.
+
+2. Verilerin Kullanımı
+
+Toplanan kişisel veriler, yalnızca Hipocapp.com platformunda sağlıkla ilgili hizmetlerin sunulması amacıyla kullanılacaktır. Kullanıcıların bilgileri, ticari amaçlar doğrultusunda üçüncü kişilerle paylaşılabilir.
+
+3. Mesajların Güvenlik Güçlerine ve Üçüncü Taraflara Verilmesi
+
+Hipocapp.com, yasal yükümlülükler gereği, kullanıcıların mesajlarını güvenlik güçlerine ve ilgili diğer üçüncü taraflara iletebilir. Bu durum, yalnızca yasal çerçevede, suç işlenmesi veya güvenlik tehditlerinin önlenmesi amacıyla gerçekleşecektir.
+
+4. Kullanıcı Sorumluluğu
+
+Kullanıcılar, platformda paylaştıkları bilgilerin doğruluğundan ve güncelliğinden sorumludur. Türkiye Cumhuriyeti'nin mevcut Kişisel Verileri Koruma Kanunu'na göre, kullanıcılar, paylaştıkları verilerin ihlali durumunda yasal sorumluluk taşıdıklarını kabul ederler.
+
+5. İzleme ve Analiz
+
+Hipocapp.com, kullanıcı deneyimini artırmak amacıyla web sayfası üzerinde izleme ve analiz yapabilir. Bu süreçte toplanan veriler, kullanıcıların gizliliğini korumak için gerekli önlemler alınarak işlenecektir.
+
+6. İlgili Yasal Düzenlemeler
+
+Bu metin, Türkiye Cumhuriyeti Kişisel Verileri Koruma Kanunu'nun (KVKK) 5. maddesi gereğince kişisel verilerin işlenmesi ve korunmasına dair düzenlemeleri kapsamaktadır. Ayrıca, 6. maddeye göre kişisel verilerin işlenmesinin şartlarını belirlemektedir. Kullanıcılar, bu metni kabul ederek, işbu kanunun bağlayıcı hükümlerine uygun hareket etmeyi kabul etmiş sayılır.
+
+Bu metin, Hipocapp.com kullanıcılarının kişisel verilerinin korunmasına dair genel bir bilgilendirme sağlar. Kullanıcılar, platforma erişim sağlayarak, yukarıda belirtilen şartları kabul etmiş sayılır.
   ''';
 }
